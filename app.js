60ec29e2d192ab9765495e3e134cecdbf2533e43
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-05-17 08:36:04 -0700

    process json

--- app.js
+++ app.js
@@ -22,4 +22,39 @@ async function getCurrentWeather(city = defaultCity) {
   }
 }
 
-getCurrentWeather().then(console.log);
+// process weather data
+function processWeatherData(apiData) {
+  const weatherData = apiData.current;
+  const locationData = apiData.location;
+
+  const appData = {
+    weather: {
+      conditionDesc: weatherData.condition.text,
+      conditionIconUrl: weatherData.condition.icon,
+      feelslike_c: weatherData.feelslike_c,
+      feelslike_f: weatherData.feelslike_f,
+      humidity: weatherData.humidity,
+      last_updated: weatherData.last_updated,
+      rain: weatherData.precip_mm,
+      temp_c: weatherData.temp_c,
+      temp_f: weatherData.temp_f,
+      wind_speed: weatherData.wind_kph,
+    },
+    location: {
+      city: locationData.name,
+      country: locationData.country,
+      lat: locationData.lat,
+      long: locationData.lon,
+      region: locationData.region,
+    },
+  };
+
+  return appData;
+}
+
+// init app
+getCurrentWeather().then((apiData) => {
+  const appData = processWeatherData(apiData);
+  console.log(appData.location);
+  console.log(appData.weather);
+});
